a621ee92ecce70c66cbb387a242504c60fad8587
faagutuy <faagutuy1446@masseyhigh.school.nz>
2021-09-02 11:43:47 +1200

    Update 02

---
+++ 03_getting_input.py
@@ -0,0 +1,12 @@
+# Ask the user for their name
+username = input("What is your name? ")
+
+# Ask the user for their favourite interger
+fav_num = int(input("Favourite Number? "))
+
+# Double, halve and square the number
+
+# Greet the user
+
+# Output the results of doubling, halving, 
+# and squaring their favoutite number
