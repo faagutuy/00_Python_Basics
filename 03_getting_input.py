4a5d4140ac70c237fd24fb951c0216f8a119927a
faagutuy <faagutuy1446@masseyhigh.school.nz>
2021-09-08 11:36:59 +1200

    update

--- 03_getting_input.py
+++ 03_getting_input.py
@@ -5,8 +5,17 @@ username = input("What is your name? ")
 fav_num = int(input("Favourite Number? "))
 
 # Double, halve and square the number
+double = fav_num * 2
+half = fav_num / 2
+squared = fav_num * fav_num
 
+print()
 # Greet the user
-
+print("Hi {}, your favourite number is {}".format(username, fav_num))
+print()
 # Output the results of doubling, halving, 
 # and squaring their favoutite number
+print("double {} is {}".format(fav_num, double))
+print("half{} is {}".format(fav_num, half))
+print ("{} squares is {}".format(fav_num, squared))
+print()
